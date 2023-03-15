bfc80f8047076d1fbf71fb52398cc1592ce577eb
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-03-15 06:46:08 +0000

    added imput validation to presonal info

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,12 +1,20 @@
 
-export function Input(props){
-	let {label,type,placeholder,value,onchange,error,success} = props;
+export function Input(props) {
+	let { label, type, placeholder, value, required,onchange,onblur,pattern, error, success } = props;
 	return <div>
-      <div>
-				<label> {label} </label>
-				{error ? <label> {error} </label>   : null }
-				{success ? <label> {success} </label> : null}
-  		</div>
-      <input type={type} placeholder={placeholder} value={value} onChange={onchange}/>
-    </div>
+		<div className="space-x-2.5">
+			<label> {label} </label>
+			{error ? <label className="text-red-500"> {error} </label> : null}
+			{success ? <label className="text-green-500"> {success} </label> : null}
+		</div>
+		<input className="block w-full px-3 py-1.5 
+			text-base font-normal 
+			bg-white bg-clip-padding 
+			border border-solid border-primary rounded 
+			transition ease-in-out m-0 
+			focus:border-tertiary focus:bg-white focus:outline-none 
+			placeholder-shown:border-gray-500
+			valid:border-green-500 invalid:border-red-500"
+			type={type} placeholder={placeholder} value={value} onChange={onchange} onBlur={onblur} required={required} pattern={pattern}/>
+	</div>
 }
--- src/views/personal-info.jsx
+++ src/views/personal-info.jsx
@@ -1,56 +1,66 @@
 import { useState } from "react";
 import { Input } from "../components/input";
 
-function inputValidation(target,setValue){
-	if(target.validity.valueMissing){
+function inputValidation(target, setValue) {
+	if (target.validity.valueMissing) {
 		setValue({
-			error:"This field should not be empty"
+			error: "This field should not be empty",
+			success: ""
 		})
 	}
-	else if(!target.validity.valid){
+	else if (!target.validity.valid) {
 		setValue({
 			value: target.value,
-			error: target.validitionMessage
+			error: target.validationMessage,
+			success: ""
 		})
 	}
 	else {
 		setValue({
 			value: target.value,
-			success: "The field is filed correctly"
+			success: "The field is filed correctly",
+			error: "",
 		})
 	}
 }
 
-export function PersonalInfo(){
-	const [name,setName]   = useState({value:undefined});
-	const [email,setEmail] = useState({value:undefined});
-	const [phone,setPhone] = useState({value:undefined});
+export function PersonalInfo() {
+	const [name, setName] = useState({ value: undefined });
+	const [email, setEmail] = useState({ value: undefined });
+	const [phone, setPhone] = useState({ value: undefined });
 
 
-	return <div class="flex flex-col gap-8">
-			<Input value={name.value} 
-							error={name.error} 
-							success={name.success}
-							onchange={(e) =>inputValidation(e.target,setName)}  
-							label="Name" 
-							type="text" 
-							placeholder="e.g. Stephen King"
-			/>
-			<Input value={email.value} 
-							error={email.error} 
-							success={email.success}
-							onchange={(e) =>inputValidation(e.target,setEmail)} 
-							label="Email Address" 
-							type="text" 
-							placeholder="e.g. stephenking@lorem.com"
-			/>
-			<Input value={phone.value} 
-							error={phone.error} 
-							success={phone.success}
-							onchange={(e) =>inputValidation(e.target,setPhone)} 
-							label="Phone Number" 
-							type="text" 
-							placeholder="e.g. +1 234 567 890"
-			/>
+	return <div className="flex flex-col gap-8">
+		<Input value={name.value}
+			error={name.error}
+			success={name.success}
+			onchange={(e) => inputValidation(e.target, setName)}
+			onblur={(e) => inputValidation(e.target, setName)}
+			label="Name"
+			type="text"
+			required
+			placeholder="e.g. Stephen King"
+		/>
+		<Input value={email.value}
+			error={email.error}
+			success={email.success}
+			onchange={(e) => inputValidation(e.target, setEmail)}
+			onblur={(e) => inputValidation(e.target, setEmail)}
+			label="Email Address"
+			type="email"
+			required
+			placeholder="e.g. stephenking@lorem.com"
+		/>
+		<Input value={phone.value}
+			error={phone.error}
+			success={phone.success}
+			onchange={(e) => inputValidation(e.target, setPhone)}
+			onblur={(e) => inputValidation(e.target, setPhone)}
+			label="Phone Number"
+			type="tel"
+			pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+			required
+			placeholder="e.g. +1 234 567 890"
+		/>
 	</div>
 }
