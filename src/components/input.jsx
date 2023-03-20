05757912f1adede2f0a6cc65507dfb7ec4797c85
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-03-20 08:14:05 +0000

    improve input style

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,20 +1,22 @@
 
 export function Input(props) {
-	let { label, type, placeholder, value, required,onchange,onblur,pattern, error, success } = props;
+	let { label, type, placeholder, value, required, onchange, onblur, pattern, error, success } = props;
 	return <div>
-		<div className="space-x-2.5">
-			<label> {label} </label>
-			{error ? <label className="text-red-500"> {error} </label> : null}
-			{success ? <label className="text-green-500"> {success} </label> : null}
+		<div className="space-x-2.5 mb-2">
+			<label className="text-gray-700 font-bold "> {label} </label>
+			{error ? <label className="text-red-500  text-xs italic"> {error} </label> : null}
+			{success ? <label className="text-green-500  text-xs italic"> {success} </label> : null}
 		</div>
-		<input className="block w-full px-3 py-1.5 
+		<input className={`block w-full px-3 py-1.5 
 			text-base font-normal 
-			bg-white bg-clip-padding 
-			border border-solid border-primary rounded 
+			border border-solid border-primary rounded-lg
 			transition ease-in-out m-0 
-			focus:border-tertiary focus:bg-white focus:outline-none 
+			focus:border-tertiary focus:bg-gray-100 focus:outline-none 
 			placeholder-shown:border-gray-500
-			valid:border-green-500 invalid:border-red-500"
-			type={type} placeholder={placeholder} value={value} onChange={onchange} onBlur={onblur} required={required} pattern={pattern}/>
+			${success ? "border-green-500 bg-green-200 focus:border-green-700"
+				: (error ? "border-red-500 bg-red-200 focus:border-red-700"
+					: "bg-gray-100"
+				)}`}
+			type={type} placeholder={placeholder} value={value} onChange={onchange} onBlur={onblur} required={required} pattern={pattern} />
 	</div>
 }
